f74fde777d4fb31b00e1f4d59e82be9aa5192c14
dgopi <dgopi@DGOPI-LAP.oradev.oraclecorp.com>
2018-08-27 13:02:30 +0530

    adding files

---
+++ src/main/java/edureka/selenium/jenkin/selenium_jenkin_proj/App.java
@@ -0,0 +1,13 @@
+package edureka.selenium.jenkin.selenium_jenkin_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/edureka/selenium/jenkin/selenium_jenkin_proj/SeleniumDemo.java
@@ -0,0 +1,50 @@
+package edureka.selenium.jenkin.selenium_jenkin_proj;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class SeleniumDemo {
+	WebDriver driver = null;
+
+	@Test
+	public void LoginTest() throws InterruptedException {
+		driver.get("http://192.168.56.101:3001/");
+		Thread.sleep(10000);
+
+		driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+
+		driver.findElement(By.name("login")).sendKeys("Edureka");
+		driver.findElement(By.name("password")).sendKeys("DevOps");
+		Thread.sleep(1000);
+
+		driver.findElement(By.name("click")).click();
+		Thread.sleep(10000);
+		Assert.assertEquals(true, true);
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out
+		.println("Hi, Welcome to Edureka's YouTube Live session on Selenium    	WebDriver");
+
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\APPs\\chromebrowser\\chromedriver.exe");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		driver = new ChromeDriver(chromeOptions);
+
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
+
+}
