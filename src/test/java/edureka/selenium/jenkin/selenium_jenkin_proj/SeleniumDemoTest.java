b8b5f7818c8e5dcd063b8bef818098ce3a82c262
dgopi <dgopi@DGOPI-LAP.oradev.oraclecorp.com>
2018-08-27 13:48:05 +0530

    adding files

--- src/test/java/edureka/selenium/jenkin/selenium_jenkin_proj/SeleniumDemoTest.java
+++ src/test/java/edureka/selenium/jenkin/selenium_jenkin_proj/SeleniumDemoTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 
-public class SeleniumDemo {
+public class SeleniumDemoTest {
 	WebDriver driver = null;
 
 	@Test
